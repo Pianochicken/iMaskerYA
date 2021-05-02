efa50002e9f836a95dd2f850b0bdbbecd6176980
Pianochicken <pianochicken1996@gmail.com>
2021-05-03 03:05:17 +0800

    Update iMaskerYA File

--- run_main.py
+++ run_main.py
@@ -14,13 +14,8 @@ first.start() # 執行該子執行緒
 time.sleep(10)
 def yolov4():
 	os.chdir('darknet/build/darknet/x64')
-	# 別人已訓練好的模型
+	# 執行Yolov4模型
 	os.system('darknet.exe detector demo obj_imaskerya.data yolov4-tiny_imaskerya.cfg yolov4-tiny_imaskerya.weights -c 0 -json_port 8070 -mjpeg_port 8090 -thresh 0.6 -dont_show')
-	# os.system('darknet.exe detector demo obj_imaskerya.data yolov4-tiny_imaskerya.cfg yolov4-tiny_imaskerya.weights -c 0 -json_port 8070 -mjpeg_port 8090 -dont_show')
-	
-	# 自己訓練的模型
-	# os.system('darknet.exe detector demo obj_20210111_change.data yolov4-tiny_20210111_change.cfg yolov4-tiny_best_20210111_change.weights -c 0 -json_port 8070 -mjpeg_port 8090 -dont_show')
-	# os.system('darknet.exe detector demo obj_20210111_no_change.data yolov4-tiny_20210111_no_change.cfg yolov4-tiny_best_20210111_no_change.weights -c 0 -json_port 8070 -mjpeg_port 8090 -dont_show')
 
 
 # # 建立第一個子執行緒
